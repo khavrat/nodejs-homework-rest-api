0fa268275c9407efd319edd6aeab562917dce69a
khavrat <ihavrat@gmail.com>
2023-05-28 20:59:23 +0300

    added mongoose's methods work

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,16 +2,21 @@ const express = require("express");
 
 const ctrl = require("../../controllers/contacts");
 
+const {isValidId} = require('../../middlewares')
+
 const router = express.Router();
 
 router.get("/", ctrl.getAll);
 
-router.get("/:id", ctrl.getById);
+router.get("/:id", isValidId, ctrl.getById);
 
 router.post("/", ctrl.add);
 
-router.delete("/:id", ctrl.deleteById);
+router.delete("/:id", isValidId, ctrl.deleteById);
 
-router.put("/:id", ctrl.updateById);
+router.put("/:id", isValidId, ctrl.updateById);
+
+router.patch("/:id/favorite", isValidId, ctrl.updateFavorite);
+
 
 module.exports = router;
