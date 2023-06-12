99d41101e0229050bfb287c7bf92e29ec76d2592
khavrat <ihavrat@gmail.com>
2023-06-12 22:16:11 +0300

    added ability to change avatar

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const ctrl = require("../../controllers/auth");
 
-const { authenticate, validateBody} = require("../../middlewares");
+const { authenticate, validateBody, upload } = require("../../middlewares");
 
 const schemas = require("../../models/user");
 
@@ -19,6 +19,8 @@ router.post("/login", validateBody(schemas.schemas.loginSchema), ctrl.login);
 router.get("/current", authenticate, ctrl.getCurrent);
 
 router.post("/logout", authenticate, ctrl.logout);
+
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar );
 
 
 module.exports = router;
